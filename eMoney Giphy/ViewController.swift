02d3f9f8a09ea6064661a93eccb508fa90577c6a
Nikoloz Tatunashvili <nikoloztatunashvili@bog.ge>
2018-08-22 23:56:55 +0400

    Initial Commit

---
+++ eMoney Giphy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eMoney Giphy
+//
+//  Created by Nikoloz Tatunashvili on 22.08.18.
+//  Copyright © 2018 Nikoloz Tatunashvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
